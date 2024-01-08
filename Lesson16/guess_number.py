1beebbd24cadfd2d575fd7f116e342e20b6b0d5c
Said Gaforov~ <sgaforov@gmail.com>
2024-01-07 17:10:20 -0800

    Mini Games Project

--- Lesson16/guess_number.py
+++ Lesson16/guess_number.py
@@ -1,32 +1,62 @@
 import random
-
+import sys
 
 def guess_number(name='PlayerOne'):
     game_count = 0
     player_wins = 0
 
-    print(f"\n{name}, let's play a game...")
-    player_guess = input(
-        '\nGuess which number I am thinking of... 1, 2, or 3.\n\n')
+    def play_guess_number():
+        nonlocal name
+        nonlocal player_wins
+        nonlocal game_count
 
-    if player_guess not in ['1', '2', '3']:
-        print('\nPlease guess a number between 1, 2, or 3')
-        return guess_number()
+        print(f"\n{name}, let's play a game...")
+        player_guess = input('\nGuess which number I am thinking of... 1, 2, or 3.\n\n')
 
-    player_guess = int(player_guess)
-    computer_guess = random.randint(1, 3)
+        if player_guess not in ['1', '2', '3']:
+            print('\nPlease guess a number between 1, 2, or 3')
+            return play_guess_number()
 
-    print('\nI was thinking about the number', computer_guess)
-    if player_guess != computer_guess:
-        print('You chose', player_guess)
+        player_guess = int(player_guess)
+        computer_guess = random.randint(1, 3)
 
-    if player_guess == computer_guess:
-        print('You also chose', player_guess)
-        print(f'\n🥳🥳🥳 You win, {name}! 🥳🥳🥳\n')
-    else:
-        print('\n🤖🤖🤖 Computer wins! 🤖🤖🤖\n')
+        print('\nI was thinking about the number', computer_guess)
 
-    return guess_number
+        if player_guess != computer_guess:
+            print('You chose', player_guess)
+
+        if player_guess == computer_guess:
+            player_wins += 1
+            print(f"You also chose {player_guess} \n\n🥳🥳🥳 You win, {name}! 🥳🥳🥳\n")
+        else:
+            print(f"\nSorry, {name}. \n🤖🤖🤖 Computer wins! 🤖🤖🤖\n")
+
+        game_count += 1
+        print(f"Game count: {game_count}")
+        print(f"{name}'s wins: {player_wins}")
+        print(f"{name}'s win percentage: {player_wins/game_count:.2%}\n")
+
+        print(f"\nPlay again, {name}?\n")
+
+        while True:
+            play_again = input('\nY for Yes or \nQ to Quit \n\n')
+            if play_again.lower() not in ['y', 'q']:
+                continue
+            else:
+                break
+
+        if play_again.lower() == 'y':
+            return play_guess_number()
+        else:
+            print(f'\n🥳 🎉 Thanks for playing!')
+            if __name__ == '__main__':
+                sys.exit('🎮🎮🎮 Exiting the game...\n\n')
+            else:
+                return
+
+
+
+    return play_guess_number
 
 
 if __name__ == '__main__':
@@ -44,4 +74,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     guess_num = guess_number(args.name)
-    # guess_num()
+    guess_num()
---
+++ Lesson16/rps.py
@@ -0,0 +1,104 @@
+import random
+import sys
+from enum import Enum
+
+# Adding ArgumentParser (command line) to make the game more personalized. 
+# Note: To achieve this, you need to be 'in' the folder and run with command line, not play/run button. Ex: python3 rps.py -n 'Said'
+
+
+def rps(name='PlayerOne'):
+    game_count = 0
+    player_wins = 0
+    computer_wins = 0
+
+    def play_rps():
+        nonlocal name
+        nonlocal player_wins
+        nonlocal computer_wins
+
+        class RockPaperScissors(Enum):
+            ROCK = 1
+            PAPER = 2
+            SCISSORS = 3
+
+        player_choice = input(
+            f'\n{name}, please enter... \n1 for Rock, \n2 for Paper, or \n3 for Scissors:\n\n')
+
+        if player_choice not in ['1', '2', '3']:
+            print('\n{name}, please enter 1, 2, or 3. Try again.')
+            return play_rps()
+
+        # I could have converted in line 23 above but for accepting user input both for int and string this way is better.
+        player = int(player_choice)
+
+        computer = int(random.choice([1, 2, 3]))
+        print(
+            f"\n{name}, you chose: {str(RockPaperScissors(player)).replace('RockPaperScissors.', '')}")
+        print(
+            f"Computer chose: {str(RockPaperScissors(computer)).replace('RockPaperScissors.', '')}")
+
+        def decide_winner(player, computer):
+            nonlocal name
+            nonlocal player_wins
+            nonlocal computer_wins
+
+            if player == computer:
+                return '\n🟰🟰 Tie game! 🟰🟰'
+            elif (
+                (player == 1 and computer == 3) or
+                (player == 3 and computer == 2) or
+                (player == 2 and computer == 1)
+            ):
+                player_wins += 1
+                return f'\n🥳🥳🥳 You win, {name}! 🥳🥳🥳'
+            else:
+                computer_wins += 1
+                return '\n🤖🤖🤖 Computer wins! 🤖🤖🤖'
+
+        game_result = decide_winner(player, computer)
+        print(game_result)
+
+        nonlocal game_count
+        game_count += 1
+
+        print('\nGame count:', game_count)
+        print(f"\n{name}'s wins: {player_wins}")
+        print('Computer wins:', computer_wins)
+
+        print('\nPlay again?')
+
+        while True:
+            play_again = input('\nY for Yes or \nQ to Quit \n\n')
+            if play_again.lower() not in ['y', 'q']:
+                continue
+            else:
+                break
+
+        if play_again.lower() == 'y':
+            return play_rps()
+        else:
+            print(f'\n🥳 🎉 Thanks for playing!')
+            if __name__ == '__main__':
+                sys.exit('🎮🎮🎮 Exiting the game...\n\n')
+            else:
+                return
+
+    return play_rps
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='Provides a personalized game experience.'
+    )
+
+    parser.add_argument(
+        '-n', '--name', metavar='name',
+        required=True, help='The name of the person playing the game.'
+    )
+
+    args = parser.parse_args()
+
+    rock_paper_scissors = rps(args.name)
+    rock_paper_scissors()
