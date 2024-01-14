b54443e39ebfad3b5a3b988e179819b421cea7d7
Said Gaforov~ <sgaforov@gmail.com>
2024-01-13 22:39:04 -0800

    Lesson 19 - Exceptions & Errors

---
+++ Lesson19/exceptions.py
@@ -0,0 +1,33 @@
+# print(a) # <-- NameError exception raised(throws)
+
+# Catching the exception
+try:
+    print(b)
+except:
+    print('There is an error')
+
+
+print('\n---------------\n')
+# Catching the exception in more specific way
+try:
+    print(c)
+except NameError as e: # 'as e' part is optional if you want to print default message. 
+    print('NameError means something probably is not defined OR Name is not defined inside paranthesis')
+    print(e)
+
+
+print('\n---------------\n')
+# Zero devision error, exception when dividing any number by zero
+x = 10
+try:
+    print(x/0)
+except ZeroDivisionError as exception:
+    print('Cannot divide by zero')
+    print(exception) # prints default message
+except Exception:
+    print("Catch errors other than 'divede by zero'")
+else:
+    print('No errors!')
+finally:
+    print('Finally block prints whether error occurs or not.')
+
---
+++ Lesson19/exceptions2.py
@@ -0,0 +1,7 @@
+a = 5
+try:
+    if not type(a) is str:
+        raise TypeError('Only strings are allowed.')
+except Exception as e:
+    print(e)
+
