753966dc828075680b5ff10b57f99adabc6ab05b
Said Gaforov~ <sgaforov@gmail.com>
2024-02-14 20:14:38 -0800

    Lesson 23 - Final Project - Flask Web App

---
+++ Lesson23/weather.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+def get_current_weather(city="Los Angeles"):
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    weather_data = requests.get(request_url).json()
+    return weather_data
+
+if __name__ == '__main__':
+    print('\n*** Get current weather conditions ***\n')
+    city = input('\nEnter a city name:')
+
+    # Checking for empty Strings or Strings with only spaces
+    if not bool(city.strip()):
+        city = "Los Angeles"
+
+
+    weather_data = get_current_weather(city)
+    print('\n')
+    pprint(weather_data)
